a443e859c822937090c849e4c53ac7c2fc1221aa
Infernaton <lubin.pauly@gmail.com>
2024-10-05 18:17:35 +0200

    Hive dying have to launch Win or Defeat from GameManager

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -13,4 +13,14 @@ public class GameManager : MonoBehaviour
         else if (Instance != this) // If there is already an instance and it's not `this` instance
             Destroy(gameObject); // Destroy the GameObject, this component is attached to
     }
+
+    public void GameOver()
+    {
+        Debug.Log("Game over");
+    }
+
+    public void Win()
+    {
+        Debug.Log("You Win");
+    }
 }
