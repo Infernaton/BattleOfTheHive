2fb5d5385e38f1bb007e577959cc7b64c1ce7d5b
Infernaton <lubin.pauly@gmail.com>
2024-10-06 16:35:17 +0200

    Add titan Model + animate with a rotation of its torus

---
+++ Assets/Scripts/Animation/TitanRotateTorus.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+class RotationOrientation
+{
+    public Transform Transform;
+    public float RotationSpeed;
+    public Vector3 RotationDirection;
+
+    public RotationOrientation(Transform t, float speed, Vector3 rotationDirection)
+    {
+        Transform = t;
+        RotationSpeed = speed;
+        RotationDirection = rotationDirection;
+    }
+}
+
+public class TitanRotateTorus : MonoBehaviour
+{
+    [SerializeField] Transform m_LargeTorus;
+    [SerializeField] List<Transform> m_RandomRotationTorus;
+    [SerializeField] float m_RotationSpeed;
+    [SerializeField] float m_OffsetSpeed;
+
+    private List<RotationOrientation> _torusRotation = new();
+
+    private void Start()
+    {
+        for (int i = 0; i < m_RandomRotationTorus.Count; i++)
+        {
+            m_RandomRotationTorus[i].rotation = Random.rotation;
+            float speed = Random.Range(m_RotationSpeed-m_OffsetSpeed, m_RotationSpeed + m_OffsetSpeed) * Utils.Random.Sign();
+            Vector3 direction = m_RandomRotationTorus[i].right;
+            _torusRotation.Add(new RotationOrientation(m_RandomRotationTorus[i], speed, direction));
+        }
+
+        _torusRotation.Add(new RotationOrientation(m_LargeTorus, m_RotationSpeed, Vector3.forward));
+    }
+
+    private void Update()
+    {
+        for (int i = 0; i < _torusRotation.Count; i++)
+        {
+            RotationOrientation r = _torusRotation[i];
+            r.Transform.Rotate(r.RotationDirection, r.RotationSpeed);
+        }
+    }
+}
--- Assets/Scripts/Utils/Utils.cs
+++ Assets/Scripts/Utils/Utils.cs
@@ -19,4 +19,15 @@ namespace Utils
             return go1.GetInstanceID() == go2.GetInstanceID();
         }
     }
+
+    public class Random
+    {
+        /**
+         * Return -1 or 1
+         */
+        public static float Sign()
+        {
+            return UnityEngine.Random.value * 2 - 1;
+        }
+    }
 }
